693cf9d0c578956e77c232dbd55e4f2c13fd258a
west040594 <semen253@mail.ru>
2018-11-08 17:51:35 +0300

    login test

--- src/test/java/com/accenture/be/business/user/UserServiceTest.java
+++ src/test/java/com/accenture/be/business/user/UserServiceTest.java
@@ -1,20 +1,22 @@
 package com.accenture.be.business.user;
 
+import com.accenture.be.business.user.exceptions.UserException;
 import com.accenture.be.business.user.implement.UserServiceImpl;
+import com.accenture.be.business.user.validators.LoginUserValidator;
 import com.accenture.be.entity.user.User;
 import com.accenture.be.repository.UserRepository;
+import com.accenture.fe.dto.user.LoginForm;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,6 +26,9 @@ public class UserServiceTest {
 
     @Mock
     UserRepository userRepository;
+
+    @Spy
+    LoginUserValidator loginUserValidator;
 
     @InjectMocks
     private UserServiceImpl userServiceImpl;
@@ -70,7 +75,45 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testLogin() {
+    public void testLogin() throws UserException {
+        when(userRepository.findByUsername("User1")).thenReturn(userList.get(0));
+        when(userRepository.findByEmail("User2Email")).thenReturn(userList.get(0));
+
+        when(userRepository.findByUsername("User2")).thenReturn(userList.get(1));
+        when(userRepository.findByEmail("User2Email")).thenReturn(userList.get(1));
+
+        //Проверка входа по логину
+        assertEquals(userList.get(0),
+                userServiceImpl.login(new LoginForm("User1", "User1Pass")));
+
+        //Проверка входа по email
+        assertEquals(userList.get(1),
+                userServiceImpl.login(new LoginForm("User2Email", "User2Pass")));
+
+        //Проверка входа не существующего пользователя
+        assertThrows(UserException.class, () -> {
+            userServiceImpl.login(new LoginForm("User5", "User5Pass"));
+        });
+
+        //Проверка входа существующего пользователя но не правильного пароля
+        assertThrows(UserException.class, () -> {
+            userServiceImpl.login(new LoginForm("User1", "User1IncorrectPass"));
+        });
+
+        //Проверка входа если в форме пустые поля
+        assertThrows(UserException.class, () -> {
+            userServiceImpl.login(new LoginForm("", ""));
+        });
+
+        //Проверка входа если в форме пароль пустой
+        assertThrows(UserException.class, () -> {
+            userServiceImpl.login(new LoginForm("User1", ""));
+        });
+
+        //Проверка входа если в форме логин пустой
+        assertThrows(UserException.class, () -> {
+            userServiceImpl.login(new LoginForm("", "User1Pass"));
+        });
 
     }
 
